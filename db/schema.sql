8108aaf8c8c180aa768c0f2f7ea597884aa1d4d2
Mike Jenkins <kinetic@icloud.com>
2016-11-17 20:43:32 +0000

    Added react boilerplate

---
+++ db/schema.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cases;
+DROP TABLE IF EXISTS criteria;
+DROP TABLE IF EXISTS alternatives;
+DROP TABLE IF EXISTS alternatives_criterias;
+
+CREATE TABLE users (
+  id BIGSERIAL PRIMARY KEY,
+  email VARCHAR(255),
+  name VARCHAR(255),
+);
+
+CREATE TABLE cases (
+  id BIGSERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  name VARCHAR(255),
+  name VARCHAR(255),
+  create_at DATE,
+  update_at DATE
+);
+
+CREATE TABLE criterias (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(75),
+  case_id INTEGER REFERENCES cases(id) ON DELETE CASCADE,
+  description VARCHAR(255),
+  low BOOLEAN,
+  parent INTEGER REFERENCES criterias(id),
+  unit_name VARCHAR(75),
+  unit_prefix VARCHAR(75),
+  unit_suffix VARCHAR(75),
+  create_at DATE,
+  update_at DATE
+);
+
+
+CREATE TABLE alternatives (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(75),
+  case_id INTEGER REFERENCES cases(id) ON DELETE CASCADE,
+  order INTEGER
+);
+
+CREATE TABLE alternatives_criterias (
+  alternative_id INTEGER REFERENCES alternatives(id) ON DELETE CASCADE,
+  criteria_id INTEGER REFERENCES criterias(id) ON DELETE CASCADE,
+  value REAL
+);
