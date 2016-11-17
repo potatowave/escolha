22600ab7134f2ee80412a040ca8d56ff339ed2ff
Paulo Brito <paulocamboim@gmail.com>
2016-11-17 13:09:42 -0800

    Update schema.sql
    Fix some typos

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS cases;
-DROP TABLE IF EXISTS criteria;
-DROP TABLE IF EXISTS alternatives;
 DROP TABLE IF EXISTS alternatives_criterias;
+DROP TABLE IF EXISTS criterias;
+DROP TABLE IF EXISTS alternatives;
+DROP TABLE IF EXISTS cases;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id BIGSERIAL PRIMARY KEY,
@@ -14,7 +14,6 @@ CREATE TABLE cases (
   id BIGSERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name VARCHAR(255),
-  name VARCHAR(255),
   create_at DATE,
   update_at DATE
 );
@@ -25,6 +24,7 @@ CREATE TABLE criterias (
   case_id INTEGER REFERENCES cases(id) ON DELETE CASCADE,
   description VARCHAR(255),
   low BOOLEAN,
+  "order" INTEGER,
   parent INTEGER REFERENCES criterias(id),
   unit_name VARCHAR(75),
   unit_prefix VARCHAR(75),
@@ -38,7 +38,7 @@ CREATE TABLE alternatives (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(75),
   case_id INTEGER REFERENCES cases(id) ON DELETE CASCADE,
-  order INTEGER
+  "order" INTEGER
 );
 
 CREATE TABLE alternatives_criterias (
