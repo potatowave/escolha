31dc840211e8ce1314f08983d93acd4a5f34102f
mahongru <mahongru.ca@gmail.com>
2016-11-18 20:03:53 +0000

    setup react/redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, {Component} from 'react';
 
 class App extends Component {
   render() {
+    console.log("Rendering <App/>");
     return (
-      <h1>Hello React :)</h1>
+      <div><h1>Hello React :)</h1></div>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,27 @@
 require("../styles/application.scss");
 
 // Render the top-level React component
+import { Provider } from 'react-redux'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'  // set up logging in the console so we can see how actions are fired
+import rootReducer from './reducers.js'
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+
+// Configure the Redux Store
+
+const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(createLogger()) // apply the 'logger' middleware. this will console.log every action that is sent to the redux store.
+)
+
+// Wrap the component in a <Provider>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
