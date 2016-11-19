cdabdba89475c9815fed672d704b0b6863642573
Paulo Brito <paulocamboim@gmail.com>
2016-11-18 20:07:39 -0800

    Add functions to add Cases, objectives, Alternatives - missing adding cell values to alternatives_objectives

--- server/routes/api/cases.js
+++ server/routes/api/cases.js
@@ -99,37 +99,162 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.post("/", (req, res) => {
-    const cases = JSON.parse(req.body.data);
-    res.send(cases.name);
+  const user_id       = 1;
 
-    knex.select('*')
-    .from('users')
-    .where({name: 'Tim'})
-    .then(function(rows) {
-    return knex.insert({user_id: rows[0].id, name: 'Test'}, 'id').into('accounts');
-    })
-    .then(function(id) {
-    console.log('Inserted Account ' + id);
-    })
-    .catch(function(error) { console.error(error); });
+  // Tracking the aSync count of rows inserted into database
+  let totalObjectives;
+  let totalAlternatives;
+
+  // Mapping id from front-end to database id in order to be able to add values
+  // Format should be: [idFrontEnd, idBackEnd]
+  let objectivesMap       = [];
+  let alternativesMap     = [];
 
 
+  /**
+  * Checking if Alternatives AND objectives were ALL added
+  * @return {object} objective - All informations about the objective
+  */
+  function isDoneInserting() {
+    // Just console log if ALL the alternatives is added to database
+    if ((alternativesMap.length == totalAlternatives) && (objectivesMap.length == totalObjectives)) {
+      return true;
+    }
 
+    return false;
+  }
+
+  /**
+  * Insert values into database
+  * @param {object}   values    - all cell values data from frontend
+  */
+
+  function insertValues(values) {
+
+    console.log("Now I can add the values in here!");
+    console.log(values);
 
     /*
-    knex
-      .select("*")
-      .from("users")
-      .where({
-        id: 1,
-      })
-      .then((results) => {
-        res.json(results);
-    });
+    knex.insert({
+      alternative_id: alternative_id,
+      objective_id:   objective_id,
+      value:          value
+    })
+    .into('alternatives_objectives')
+    .then( (objective_id) => {
+
+    })
+    .catch(function(error) { console.error(error); });
     */
+  }
 
 
+  /**
+  * Insert an objetive into database
+  * @param {object}   objective - All informations about the objective
+  * @param {integer}  case_id   - To which course objective belongs
+  * @param {object}   values    - all cell values data from frontend
+  */
+
+  function insertObjective(objective, case_id, order, values) {
+
+    knex.insert({
+      name:                   objective.objective,
+      sub_name:               objective.subObjective,
+      case_id:                parseInt(case_id),
+      evaluation_objective:   objective.criterion,
+      low_is_better:          objective.low_is_better,
+      order:                  parseInt(order),
+      unit_name:              objective.unit_name,
+      unit_prefix:            objective.unit_prefix,
+      unit_suffix:            objective.unit_suffix,
+    }, 'id')
+    .into('objectives')
+    .then( (objective_id) => {
+
+      // Maping id_front-end to the new id from database
+      objectivesMap.push([objective.id_frontend, objective_id[0]]);
+
+      // Insert to alternatives_objectives only if all other data is alredy
+      // inserted
+      if (isDoneInserting()) {
+        insertValues(values)
+      }
+    })
+    .catch(function(error) { console.error(error); });
+  }
+
+  /**
+  * Insert an alternative to database
+  * @param {object} objective - All informations about the objective
+  * @param {integer} case_id  - To which course objective belongs
+  * @param {object}   values    - all cell values data from frontend
+  */
+  function insertAlternative(alternative, case_id, order, values) {
+
+    knex.insert({
+      case_id:                parseInt(case_id),
+      name:                   alternative.name,
+      image_url:              alternative.image_url,
+      order:                  parseInt(order),
+    }, 'id')
+    .into('alternatives')
+    .then( (alternative_id) => {
+
+      // Maping id_front-end to the new id from database
+      alternativesMap.push([alternative.id_frontend, alternative_id[0]]);
+
+      // Insert to alternatives_objectives only if all other data is alredy
+      // inserted
+      if (isDoneInserting()) {
+        insertValues(values)
+      }
+    })
+    .catch(function(error) { console.error(error); });
+  }
+
+
+  /**
+  * Insert a case
+  * @param {object} data      - Json data with all case data
+  * @param {integer} user_id  - Which user own the case
+  */
+  function insertCase(data, user_id) {
+
+    knex.insert({
+      user_id: user_id,
+      name: data.name,
+      description: data.description
+    }, 'id')
+    .into('cases')
+    .then( (case_id) => {
+
+      // To be track of numbers of aSync request and know when is done
+      totalObjectives   = data.objectives.length;
+      totalAlternatives = data.alternatives.length;
+
+      // Add Objectives
+      data.objectives.forEach((objective, index) => {
+        let order = index + 1;
+        insertObjective(objective, case_id, order, data.values)
+      });
+
+      // Add alternatives
+      data.alternatives.forEach((alternative, index) => {
+        let order = index + 1;
+        insertAlternative(alternative, case_id, order, data.values)
+      });
+
+    })
+    .catch(function(error) { console.error(error); });
+  }
+
+  // --------------------------------------------------------------------------
+  // POST ROUTE
+
+  router.post("/", (req, res) => {
+    const data = JSON.parse(req.body.data);
+    insertCase(data, user_id);
   });
 
   return router;
