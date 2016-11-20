06574a3f96452db01a30fad5f875410cc3c241b1
Mike Jenkins <kinetic@icloud.com>
2016-11-20 20:34:16 +0000

    Delinted

--- server/express_server.js
+++ server/express_server.js
@@ -1,17 +1,19 @@
-"use strict";
+
 
 require('dotenv').config();
 
-const express           = require('express');
-const bodyParser        = require('body-parser');
-const randString        = require('./scripts/randomstring');
-const webpack           = require('webpack');
-const WebpackDevServer  = require('webpack-dev-server');
-const config            = require('./webpack.config');
-const ENV               = process.env.ENV || "development";
-const knexConfig        = require("./knexfile.js");
-const knex              = require("knex")(knexConfig[ENV]);
-const app               = express();
+const ENV = process.env.ENV || 'development';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const randString = require('./scripts/randomstring');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+const knexConfig = require('./knexfile.js');
+const knex = require('knex')(knexConfig[ENV]);
+
+const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,8 +38,9 @@ new WebpackDevServer(webpack(config), {
 // API - Server
 
 // Routes
-const apiCasesRoute = require("./routes/api/cases.js");
-app.use("/api/cases", apiCasesRoute(knex));
+const apiCasesRoute = require('./routes/api/cases.js');
+
+app.use('/api/cases', apiCasesRoute(knex));
 
 const PORT = process.env.PORT || 3001; // set to 3001
 app.set('view engine', 'ejs'); // Set View Engine to ejs
