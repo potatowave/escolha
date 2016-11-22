5789a85af21e1bd898b9344a0bf162b42e89e3a6
Paulo Brito <paulocamboim@gmail.com>
2016-11-21 19:42:45 -0800

    Allow CORS in the server

--- server/express_server.js
+++ server/express_server.js
@@ -15,8 +15,13 @@ const knex = require('knex')(knexConfig[ENV]);
 
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // ----------------------------------------------------------------------------
 // Webpack - Server
