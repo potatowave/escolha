e4015a404af94b52dbde9ddf61c2911a90dcee3f
Christian Beaudrie <bowtree00@gmail.com>
2016-11-22 05:06:55 +0000

    Added comments in Cells.jsx

--- src/Cells.jsx
+++ src/Cells.jsx
@@ -21,7 +21,15 @@ class Cells extends Component {
                 const selected_value = "xxxxxx";
               
                 if (this.props.low_is_better) {
-                  
+                  // put logic here 
+            
+                  // Put in uistate the alternative_id of the selected column
+                  // Then get the value of the cell in that column for the current objective row
+                  // Use that to compare against
+                  // If value is lower that selected value, give it a tag called 'better' - add CSS to make 'better' GREEN
+                  // If value is higher, give it a tag called 'worse' - add CSS to make 'worse' RED
+                  // For the 'else' statement below, do the opposite colour code.
+                  // STRETCH - test if the value is > or < 10% (or a user settable threshold) different - colour it WHITE if it is within that threshold
 
                 } else {
 
