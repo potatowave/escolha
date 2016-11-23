d8e338dec245addf82cd9cde495b9df8867c85be
Christian Beaudrie <bowtree00@gmail.com>
2016-11-23 03:27:12 +0000

    minor refactoring Cells.jsx

--- src/Cells.jsx
+++ src/Cells.jsx
@@ -6,85 +6,62 @@ class Cells extends Component {
   render() {
     console.log("Rendering <Cells />");
 
-
-    function findSelectedValues(item) {
-       return item.alternative_id === this.props.alt;
+    function findSelectedCells(item) {
+      return item.alternative_id === this.props.alt;
     }
 
     function matchingPropsRow(item) {
       return item.objective_id === this.props.row;
     }
 
-    var thisRowsSelectedValue = this.props.cells.filter(findSelectedValues, this).find(matchingPropsRow, this);
-    console.log("selected_value", thisRowsSelectedValue)
-    // var selected_value = 130000
+    // Grab all cells in the current row
+    const selectedCells = this.props.cells.filter(findSelectedCells, this);
+    console.log("selectedCells: ", selectedCells)
 
+    // Grab the selected value for the current row
+    var thisRowsSelectedValue = selectedCells.find(matchingPropsRow, this);
+
+    console.log("thisRowsSelectedValue: ", thisRowsSelectedValue)
+
+    // var thisRowsSelectedValue = this.props.cells.filter(findSelectedCells, this).find(matchingPropsRow, this);
+    // console.log("selected_value", thisRowsSelectedValue)
 
     return (
-        <div className={"r"+this.props.row}>
+      <div className={"r"+this.props.row}>
 
-          { this.props.cells.filter(matchingPropsRow, this).map((item) => {
+      { this.props.cells.filter(matchingPropsRow, this).map((item) => {
 
-            // grab selected_value from the store
+        var test = ""
 
+        if (item.alternative_id === this.props.alt) {
+          test = "highlight";
+        }
 
-              //var selected_value = 130000
+        var compare_tag = ""
 
+        if (this.props.low_is_better) {
+          
+          if (item.value < thisRowsSelectedValue.value) {
+            compare_tag = "better"
+          } else if (item.value > thisRowsSelectedValue.value) {
+            compare_tag = "worse"
+          }
 
-              // if(this.props.row === item.objective_id) {
+        } else {
+          if (item.value > thisRowsSelectedValue.value) {
+            compare_tag = "better"
+          } else if (item.value < thisRowsSelectedValue.value) {
+            compare_tag = "worse"
+          }
+        }
 
+        var stuff = <div key={`c${item.alternative_id}-${item.alternative_id}`} className={"c"+item.alternative_id+" "+test+" "+compare_tag}>{item.value}</div>
 
-                var test = ""
+        return stuff
 
-                if (item.alternative_id === this.props.alt) {
-                  test = "highlight";
-                }
+      })}
 
-                var compare_tag = ""
-
-                // var thisRowsSelectedValue = selectedValuesAcrossRows.find(matchingRow, this);
-
-                  if (this.props.low_is_better) {
-                  // put logic here
-
-                  // Put in uistate the alternative_id of the selected column
-                  // Then get the value of the cell in that column for the current objective row
-                  if (item.value < thisRowsSelectedValue.value) {
-                    compare_tag = "better"
-                  } else if (item.value > thisRowsSelectedValue.value) {
-                    compare_tag = "worse"
-                  }
-                  // Use that to compare against
-                  // If value is lower that selected value, give it a tag called 'better' - add CSS to make 'better' GREEN
-                  // If value is higher, give it a tag called 'worse' - add CSS to make 'worse' RED
-                  // For the 'else' statement below, do the opposite colour code.
-                  // STRETCH - test if the value is > or < 10% (or a user settable threshold) different - colour it WHITE if it is within that threshold
-
-                } else {
-                  if (item.value > thisRowsSelectedValue.value) {
-                    compare_tag = "better"
-                  } else if (item.value < thisRowsSelectedValue.value) {
-                    compare_tag = "worse"
-                  }
-                }
-
-                // }, this);
-
-                // var junk = this.props.low_is_better;
-
-                // if (this.props.lower_is_better === false) {
-                //   junk = "lower";
-                // }
-
-                var stuff = <div key={`c${item.alternative_id}-${item.alternative_id}`} className={"c"+item.alternative_id+" "+test+" "+compare_tag}>{item.value}</div>
-
-                // var stuff = <div key={`c-${item.objective_id}-${item.alternative_id}`} className={"c"+item.alternative_id+" "+test+" "+junk}>{item.value}</div>
-
-                return stuff
-              // }
-            })}
-        </div>
-
+      </div>
     );
   }
 }
@@ -104,7 +81,6 @@ function mapDispatchToProps(dispatch) {
   return {
     saveSelectedValue: function(value) {
       const action = { type: 'saveSelectedVal', value: value };
-      // alert('here');
       dispatch(action);
     }
   }
