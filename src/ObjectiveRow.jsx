773c83cd1bc77f222a9dd089d6ddbf1f8de0e550
Mike Jenkins <kinetic@icloud.com>
2016-11-27 05:34:13 +0000

    Re-added ObjectiveRow.jsx

--- src/ObjectiveRow.jsx
+++ src/ObjectiveRow.jsx
@@ -33,7 +33,6 @@ class ObjectiveRow extends Component {
             cell={cell}
             cell_index={index}
             thisRowsSelectedValue={thisRowsSelectedValue}
-
             cellBeingEdited={this.props.cellBeingEdited}
             cellToggled={this.props.cellToggled}
             cellSave={this.props.cellSave}
