df093bee71780bc5d8fea8e55aa4088e3efded56
Christian Beaudrie <bowtree00@gmail.com>
2016-11-30 21:07:37 +0000

    updated app.html and webpack.config.js so API server gets bundle.js from the webpack dev server on 3000, rather than from a folder

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,7 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-var WriteFilePlugin = require('write-file-webpack-plugin');
-
 
 var reactDomLibPath = path.join(__dirname, "./node_modules/react-dom/lib");
 var alias = {};
@@ -11,28 +9,20 @@ var alias = {};
 });
 
 module.exports = {
-  devServer: {
-    outputPath: path.join(__dirname, './public/assets/js'),
-    // outputPath: path.join(__dirname, './dist'),
-    // filename: 'bundle.js',
-    // publicPath: '/build/'
-  },
-  output: {
-    path: path.join(__dirname, './public/assets/js'),
-    // filename: 'bundle.js',
-    // publicPath: '/build/'
-    // publicPath: '/server/public/assets/js/'
-  },
-  plugins: [
-      new WriteFilePlugin()
-  ],
   devtool: 'eval',
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     './src/index.jsx'
   ],
+  output: {
+    path: path.join(__dirname, 'dist'), 
+    filename: 'bundle.js',
+    publicPath: '/build/'// This is the config for where we want the dev server to pretend where the file is
+    // publicPath: '/server/public/assets/js/'
+  },
   module: {
     loaders: [
+
       {
         test: /\.jsx?$/,
         loader: 'babel',
