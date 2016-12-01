17f67a15419b45e90498982ba77c9d9083eda189
Paulo Brito <paulocamboim@gmail.com>
2016-12-01 00:11:37 -0800

    Change config of production server

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_PRODUCTION + '?ssl=true',
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
